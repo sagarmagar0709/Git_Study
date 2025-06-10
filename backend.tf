3bf5bfc5333c36c8cc7704880e2697b6e01a13bd
sagarmagar0709 <sagarmagar0709@gmail.com>
2025-06-10 14:46:35 +0530

    tf newcode123

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "sagar-demo-bucket-0012"
     key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
